553189433a6477f3d462607e44961bc6cb9db59d
Aric <aric@the5flors.com>
2020-03-09 11:17:36 -0400

    Coding done, now to test, comment, and document

--- checkData.py
+++ checkData.py
@@ -1,8 +1,29 @@
 #!/bin/env python
+import sys
 import os
 from shutil import move
 
-def isValid(filename)
+def isValid(filename):
+    file = open(filename, "r")
+    if "Keyword" in file.read():
+        file.close()
+        return False
+    else:
+        file.close()
+        return True
     
+def main():
+    print('Sorting files...')
+    argList = sys.argv
+    argList.remove("checkData.py")
+    fileList = [s.replace("data/", "") for s in argList]
 
+    for i in range(0, len(fileList)):
+        if(isValid(argList[i])):
+            move(argList[i], "valid/" + fileList[i])
+        else:
+            move(argList[i], "invalid/" + fileList[i])
+    
+    print('Sorted.')
 
+main()
---
+++ reset.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+mv valid/* data/
+mv invalid/* data/
